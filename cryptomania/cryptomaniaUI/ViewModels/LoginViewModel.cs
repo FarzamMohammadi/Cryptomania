088269b96fcab446ca080efdd9551c923807f90f
Farzam Mohammadi <72108716+FarzamMohammadi@users.noreply.github.com>
2021-11-25 01:24:52 -0500

    WPF frontend template created with seamless window change capability

---
+++ cryptomania/cryptomaniaUI/ViewModels/LoginViewModel.cs
@@ -0,0 +1,36 @@
+﻿using cryptomaniaUI.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace cryptomaniaUI.ViewModels
+{
+    class LoginViewModel : BaseViewModel, IPageViewModel
+    {
+        private ICommand _goToSignUp;
+        private ICommand _goToMain;
+
+
+        public ICommand GoToSignUp
+        {
+            get
+            {
+                return _goToSignUp ?? (_goToSignUp = new RelayCommand(x =>
+                {
+                    Mediator.Notify("GoToSignUpView", "");
+                }));
+            }
+        }
+        public ICommand GoToMain
+        {
+            get
+            {
+                return _goToMain ?? (_goToMain = new RelayCommand(x =>
+                {
+                    Mediator.Notify("GoToHomeView", "");
+                }));
+            }
+        }
+    }
+}
---
+++ cryptomania/cryptomaniaUI/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,73 @@
+﻿using cryptomaniaUI.Commands;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace cryptomaniaUI.ViewModels
+{
+    public class MainWindowViewModel : BaseViewModel
+    {
+        private IPageViewModel _currentPageViewModel;
+        private List<IPageViewModel> _pageViewModels;
+
+        public List<IPageViewModel> PageViewModels
+        {
+            get
+            {
+                if (_pageViewModels == null)
+                    _pageViewModels = new List<IPageViewModel>();
+
+                return _pageViewModels;
+            }
+        }
+
+        public IPageViewModel CurrentPageViewModel
+        {
+            get
+            {
+                return _currentPageViewModel;
+            }
+            set
+            {
+                _currentPageViewModel = value;
+                OnPropertyChanged("CurrentPageViewModel");
+            }
+        }
+
+        private void ChangeViewModel(IPageViewModel viewModel)
+        {
+            if (!PageViewModels.Contains(viewModel))
+                PageViewModels.Add(viewModel);
+
+            CurrentPageViewModel = PageViewModels
+                .FirstOrDefault(vm => vm == viewModel);
+        }
+
+        private void OnGo2MainScreen(object obj)
+        {
+            ChangeViewModel(PageViewModels[0]);
+        }
+
+        private void OnGo2SignUpScreen(object obj)
+        {
+            ChangeViewModel(PageViewModels[2]);
+        }
+        private void OnGo2LoginScreen(object obj)
+        {
+            ChangeViewModel(PageViewModels[1]);
+        }
+
+        public MainWindowViewModel()
+        {
+            // Add available pages and set page
+            PageViewModels.Add(new HomeViewModel());
+            PageViewModels.Add(new LoginViewModel());
+            PageViewModels.Add(new SignUpViewModel());
+
+            CurrentPageViewModel = PageViewModels[0];
+
+            Mediator.Subscribe("GoToHomeView", OnGo2MainScreen);
+            Mediator.Subscribe("GoToLoginView", OnGo2LoginScreen);
+            Mediator.Subscribe("GoToSignUpView", OnGo2SignUpScreen);
+        }
+    }
+}
