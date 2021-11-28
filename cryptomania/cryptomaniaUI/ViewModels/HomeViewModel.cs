c763abf030cd7ddcf2db6de4d8eb93d4891740cb
Curtis Calina <74023044+ccalina@users.noreply.github.com>
2021-11-28 03:43:25 -0500

    cryptomania ui changes
    updated a few views and updated some functionality of the buttons.

---
+++ cryptomania/cryptomaniaUI/ViewModels/HomeViewModel.cs
@@ -0,0 +1,34 @@
+﻿using cryptomaniaUI.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace cryptomaniaUI.ViewModels
+{
+    class HomeViewModel : BaseViewModel, IPageViewModel
+    {
+        private ICommand _goToLogin;
+        private ICommand _goToSignUp;
+        public ICommand GoToLogin
+        {
+            get
+            {
+                return _goToLogin ?? (_goToLogin = new RelayCommand(x =>
+                {
+                    Mediator.Notify("GoToLoginView", "");
+                }));
+            }
+        }
+        public ICommand GoToSignUp
+        {
+            get
+            {
+                return _goToSignUp ?? (_goToSignUp = new RelayCommand(x =>
+                {
+                    Mediator.Notify("GoToHomeView", "");
+                }));
+            }
+        }
+    }
+}
